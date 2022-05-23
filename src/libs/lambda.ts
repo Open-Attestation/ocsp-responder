0a28ab0079278c43545fc8e032469e9deea49bef
Kyle Huang Junyuan <kyle@hjunyuan.com>
2022-05-23 18:09:53 +0800

    feat: add @middy/http-error-handler

--- src/libs/lambda.ts
+++ src/libs/lambda.ts
@@ -1,6 +1,7 @@
-import middy from "@middy/core"
-import middyJsonBodyParser from "@middy/http-json-body-parser"
+import middy from "@middy/core";
+import middyJsonBodyParser from "@middy/http-json-body-parser";
+import middyErrorHandler from "@middy/http-error-handler";
 
 export const middyfy = (handler) => {
-  return middy(handler).use(middyJsonBodyParser())
-}
+  return middy(handler).use([middyJsonBodyParser(), middyErrorHandler()]);
+};
