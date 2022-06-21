11f99339700a98ebe9b5da30abcb1cd22c9daae8
Kyle Huang Junyuan <kyle@hjunyuan.com>
2022-06-21 17:50:41 +0800

    fix: enable cors for GET endpoint (#6)
    * fix: set Access-Control-Allow-Origin=* on GET endpoint
    
    * fix: change cors back to true
    
    * chore: npm i @middy/http-cors
    
    * fix: add cors middleware
    
    Co-authored-by: David <david_tjokroaminoto@tech.gov.sg>

--- src/libs/lambda.ts
+++ src/libs/lambda.ts
@@ -1,7 +1,12 @@
 import middy from "@middy/core";
+import middyCors from "@middy/http-cors";
 import middyJsonBodyParser from "@middy/http-json-body-parser";
 import middyErrorHandler from "@middy/http-error-handler";
 
 export const middyfy = (handler) => {
-  return middy(handler).use([middyJsonBodyParser(), middyErrorHandler()]);
+  return middy(handler).use([
+    middyJsonBodyParser(),
+    middyErrorHandler(),
+    middyCors(),
+  ]);
 };
