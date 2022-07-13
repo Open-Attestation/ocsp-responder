1d31091186e84ef1f51beb12cbf1f31d10cfa9d5
Kyle Huang Junyuan <kyle@hjunyuan.com>
2022-07-13 10:44:47 +0800

    chore: reformat code using prettier

--- src/libs/lambda.ts
+++ src/libs/lambda.ts
@@ -4,9 +4,5 @@ import middyJsonBodyParser from "@middy/http-json-body-parser";
 import middyErrorHandler from "@middy/http-error-handler";
 
 export const middyfy = (handler) => {
-  return middy(handler).use([
-    middyJsonBodyParser(),
-    middyErrorHandler(),
-    middyCors(),
-  ]);
+  return middy(handler).use([middyJsonBodyParser(), middyErrorHandler(), middyCors()]);
 };
